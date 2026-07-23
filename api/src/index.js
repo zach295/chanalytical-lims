01f8672b1777d0ceca342fb015e8cc67acf9dcc4
zach295 <zach@chanalytical.com>
2026-07-23 15:28:24 -0400

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,7 @@
 // Register all Azure Functions
+require('./clients-read');
 require('./get-scan-queue');
 require('./accession-status');
-require('./clients-read');
 require('./clients-write');
 require('./users-manage');
 require('./reject-sample');
@@ -13,5 +13,3 @@ require('./mark-scan-processed');
 require('./test-types');
 require('./admin');
 require('./setup-lists');
-require('./scan-folder');
-require('./generate-report');
