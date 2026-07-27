5ae37140bf58ba5a2924a0d2f4db2c4637cf8ef5
zach295 <zach@chanalytical.com>
2026-07-27 15:21:07 -0400

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -1,3 +1,16 @@
 require('./get-scan-queue');
 require('./accession-status');
 require('./clients-read');
+require('./get-scan-queue');
+require('./accession-status');
+require('./clients-write');
+require('./users-manage');
+require('./reject-sample');
+require('./get-rejections');
+require('./lookup-labid');
+require('./cache-results');
+require('./approve-scan');
+require('./mark-scan-processed');
+require('./test-types');
+require('./admin');
+require('./setup-lists');
