4192ca925185d78c7cb2528fc1766c6220df5146
zach295 <zach@chanalytical.com>
2026-07-27 14:16:07 -0400

    Update index.js

--- api/src/index.js
+++ api/src/index.js
@@ -17,6 +17,3 @@ require('./generate-report');
 require('./control-sheet');
 require('./update-sample');
 require('./send-report');
-require('./render-report-pdf');
-require('./update-inventory');
-require('./sync-to-sheets');
